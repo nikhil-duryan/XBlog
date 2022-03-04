f1cec0a9c2a852b80e73909f4f885a7c2f77f272
nikhil-duryan <87350951+nikhil-duryan@users.noreply.github.com>
2022-03-04 12:44:39 +0530

    Update settings.py

--- XBlog/settings.py
+++ XBlog/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6eyt5kdr1qjz#u@5(429eszm%sz9ns^ji$9wu#b=m-@c*^s@p^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','xblog1.herokuapp.com']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
