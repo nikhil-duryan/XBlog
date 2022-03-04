9721ebf048814a0b3f05d0e961d53ce61786dac8
nikhil-duryan <87350951+nikhil-duryan@users.noreply.github.com>
2022-03-04 13:10:34 +0530

    Update settings.py

--- XBlog/settings.py
+++ XBlog/settings.py
@@ -125,8 +125,9 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"),
+   
 ]
-
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static'])
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
