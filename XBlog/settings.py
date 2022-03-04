7b06c59d584305f0b3dca97656b60377e8cb5ecf
nikhil-duryan <87350951+nikhil-duryan@users.noreply.github.com>
2022-03-04 13:33:22 +0530

    Update settings.py

--- XBlog/settings.py
+++ XBlog/settings.py
@@ -122,8 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static]))
-                                                              
+                                                             
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"),
    
