197d0e79373235c5606cd49d859cf0550662fa3e
nikhil-duryan <87350951+nikhil-duryan@users.noreply.github.com>
2022-03-04 12:48:34 +0530

    Update urls.py

--- XBlog/urls.py
+++ XBlog/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 admin.site.site_header="XBlog Admin"
 admin.site.site_title="XBlog Admin Panel"
@@ -26,6 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls')),
     path('Blog/', include('Blog.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
